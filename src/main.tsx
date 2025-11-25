f6fd1c72af607ace2f77857f5556f9e95f05bb9e
Igor <guedes.igor018@gmail.com>
2025-11-25 13:15:25 -0300

    teste vercel

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import './styles/globals.css';
 
 import { ThemeProvider } from '@/components/theme-provider';
 
+console.log('VITE_SUPABASE_URL =', import.meta.env.VITE_SUPABASE_URL);
+console.log('VITE_SUPABASE_ANON_KEY =', import.meta.env.VITE_SUPABASE_ANON_KEY);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ThemeProvider>
