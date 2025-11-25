60f103f81e22e33842efc2a3398e74221fd2fce2
Igor <guedes.igor018@gmail.com>
2025-11-25 13:13:06 -0300

    teste vercel

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,6 +6,9 @@ import AppRoutes from './routes';
 
 const queryClient = new QueryClient();
 
+console.log('VITE_SUPABASE_URL =', import.meta.env.VITE_SUPABASE_URL);
+console.log('VITE_SUPABASE_ANON_KEY =', import.meta.env.VITE_SUPABASE_ANON_KEY);
+
 export default function App() {
     return (
         <QueryClientProvider client={queryClient}>
